a9a440cf8790e474fb371c16b24efd0d2c63cd96
Kaine <kainedavidobrien@gmail.com>
2025-08-22 15:56:50 +1000

    walking

--- Assets/Scripts/AIMove.cs
+++ Assets/Scripts/AIMove.cs
@@ -1,24 +1,29 @@
 using NUnit.Framework;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class AIMove : MonoBehaviour
 {
     public Transform[] markers;
-    private int destPoint = 0;
     NavMeshAgent agent;
-
+    public bool currentDestination;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-
         agent.autoBraking = false;
     }
 
-    void MoveToMarkers()
+    private void Update()
     {
-        
+        if (agent.remainingDistance < 0.2)
+        {
+            Transform destination = markers[0];
+            if (currentDestination) destination = markers[1];
+            agent.destination = destination.position;
+            currentDestination = !currentDestination;
+        }
     }
 }
