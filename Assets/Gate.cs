ee063ab32721b4815ae60d5fe8ec7cfc501373ef
chrisewoi <69186935+chrisewoi@users.noreply.github.com>
2025-08-30 17:05:14 +1000

    Gate explodey, level is prefab

---
+++ Assets/Gate.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    private bool unexploded;
+
+    public GameObject gatePieces;
+    private GameObject player;
+
+    private Vector3 blastDirection;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        unexploded = true;
+    }
+
+    void Explode()
+    {
+        unexploded = false;
+        blastDirection = transform.position - player.transform.position;
+        GameObject gate2 = Instantiate(gatePieces, transform.position, transform.rotation);
+        Destroy(gameObject);
+        foreach (Rigidbody rb in gate2.GetComponentsInChildren<Rigidbody>())
+        {
+            rb.AddForce(blastDirection * 10, ForceMode.Impulse);
+        }
+        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (unexploded && other.CompareTag("Player"))
+        {
+            player = other.gameObject;
+            Explode();
+        }
+    }
+}
